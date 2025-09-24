e4fd1ace37ecc2de7dc63dd2ba83d14a81e65468
Sweetyamnesia <angela.r@live.fr>
2025-09-24 10:00:47 +0200

    Fix 1-print_square.js: parse size as decimal and validate input

--- challenge/1-print_square.js
+++ challenge/1-print_square.js
@@ -14,7 +14,11 @@ if (process.argv.length <= 2) {
     process.exit(1)
 }
 
-size = parseInt(process.argv[2], 16)
+let size = parseInt(process.argv[2], 10)
+
+if (isNaN(size) || size <= 0) {
+    process.exit(1);
+}
 
 for (let i = 0 ; i < size ; i ++) {
     for (let j = 0 ; j < size ; j ++) {
